2b3e3bb47caedbd43a2cecd83a73d5cce4331377
Meiryelle <meirymacedo.35@gmail.com>
2025-10-19 00:07:14 -0300

    fix: corrigir import circular e inicializar db

--- src/app.py
+++ src/app.py
@@ -1,10 +1,23 @@
 
-from . import create_app
+from flask import Flask
 
-app = create_app()
+from .config import db
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    from .config import Config
+    app.config.from_object(Config)
+    from .models import Usuario
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
 
 
 
