eee52a77e1988cfb3d6d77aedde8d5756859ec7a
Meiryelle <meirymacedo.35@gmail.com>
2025-10-19 00:07:50 -0300

    feat: adicionado nome da tabela

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
-from app import db
+from .app import db
 
 class Usuario(db.Model):
+    __tablename__ = 'usuario'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
