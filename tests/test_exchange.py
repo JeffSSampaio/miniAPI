2b1dd6e8efe15485b2ab05843f6b98b36ad345b8
Matheus <matheustarcio2405@gmail.com>
2025-11-20 20:17:36 -0300

    test(exchange): add mocked awesomeapi test

---
+++ tests/test_exchange.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+from src.services.exchange_service import get_usd_to_brl
+
+@patch("src.services.exchange_service.requests.get")
+def test_get_usd_to_brl(mock_get):
+    mock_get.return_value.json.return_value = {
+        "USDBRL": {
+            "bid": "5.25"
+        }
+    }
+
+    result = get_usd_to_brl()
+
+    assert result["usd"] == "5.25"
